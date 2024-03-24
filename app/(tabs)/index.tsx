29f37ba4cda15adb2011ebb9549e71d7dbbabd2a
temmie <lele.haoling2017@gmail.com>
2024-03-24 22:02:12 +0100

    configure tsconfig

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,8 +2,8 @@ import { StyleSheet, Image } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
-import products from '../../assets/data/products'
-import MenuItem from '../../components/MenuItem'
+import products from '@assets/data/products'
+import MenuItem from '@components/MenuItem'
 export default function MenuScreen() {
   return (
     <View>
