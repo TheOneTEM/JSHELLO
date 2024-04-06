35e8892c401662365ca51d39b7ab220e43a9848e
Alvin <lele.haolin2017@gmail.com>
2024-04-06 23:57:35 +0200

    fleshed out menu details screen

--- app/(tabs)/menu/[id].tsx
+++ app/(tabs)/menu/[id].tsx
@@ -1,12 +1,44 @@
-import {View, Text} from 'react-native';
-import {useLocalSearchParams, Stack} from 'expo-router'
+import {View, Text, Image, StyleSheet} from 'react-native';
+import {useLocalSearchParams, Stack} from 'expo-router';
+import products from '@assets/data/products';
+import {DefaultImage} from '@/components/MenuItem';
 
 export default function ProductDetailsScreen(){
 	const {id}=useLocalSearchParams();
+	const product = products.find((p) => p.id.toString() == id);
+	if(!product){
+		return (
+			<View>
+                <Stack.Screen options = {{title:'Not Found!'}}/>
+                <Text style={{fontSize:20}}>Product not found!</Text>
+            </View>
+        );
+	};
     return(
-        <View>
-            <Stack.Screen options = {{title:'Details:'+id}}/>
-            <Text style={{fontSize:20}}>Screen for product id: {id}</Text>
+        <View style= {styles.container}>
+            <Stack.Screen options = {{title:product.name}}/>
+            <Image
+                source={{uri: product.image || DefaultImage}}
+                style = {styles.image}
+                resizeMode = "contain"
+            />
+            <Text style={styles.price}>${product.price}</Text>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: '#ffffff',
+		flex:1,
+		padding: 10,
+	},
+	image: {
+		width: '100%',
+		aspectRatio: '1',
+	},
+	price: {
+		fontSize: 18,
+		fontWeight: 'bold',
+	},
+})
